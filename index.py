df2c719eca5c98941ff07bb9adeb5861a641c77e
Kartik Pandey <kartikpandey596@gmail.com>
2025-03-08 17:23:40 +0100

    push

--- index.py
+++ index.py
@@ -7,5 +7,6 @@ st.title('Content Extractor Website')
 with st.form("my_form"):
     user_name = st.text_input("Enter your WebURL:")
     submit_button = st.form_submit_button("Submit")
+    
 if submit_button:
     st.write(f"Welcome, {user_name}!") 
