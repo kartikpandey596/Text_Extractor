7c4759830fe4018d4dbe0b5495839c460e8feb74
Kartik Pandey <kartikpandey596@gmail.com>
2025-03-08 17:45:08 +0100

    poetry

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import validators
+import requests
+from bs4 import BeautifulSoup
 
 st.title('Content Extractor Website')
 
@@ -8,7 +10,7 @@ with st.form("my_form"):
     submit_button = st.form_submit_button("Submit")
 
 if submit_button:
-    if validators.url(weburl) is True
+    if validators.url(weburl) is True:
         st.write(weburl)
     else:
         st.write("Invalid URL")
