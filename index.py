c53c9c836be98694ca58fe29625ef63bf809d282
Kartik Pandey <kartikpandey596@gmail.com>
2025-03-08 17:22:57 +0100

    push index.py

--- index.py
+++ index.py
@@ -2,4 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('Content Extractor Website')
+
+with st.form("my_form"):
+    user_name = st.text_input("Enter your WebURL:")
+    submit_button = st.form_submit_button("Submit")
+if submit_button:
+    st.write(f"Welcome, {user_name}!") 
